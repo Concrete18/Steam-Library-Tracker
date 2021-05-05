260c3391e60fd7d6bb2a2b030f4c07c782e05112
Concrete18 <michaelericson19@gmail.com>
2021-05-05 14:57:07 -0400

    added get_time_to_beat function and reordered some code in refresh_steam_games function

--- Game_Tracker.py
+++ Game_Tracker.py
@@ -1,7 +1,8 @@
-import openpyxl
 from openpyxl.styles.borders import Side
-import datetime as dt
+import openpyxl
 import requests, random, shutil, time, json, os, re
+import datetime as dt
+from howlongtobeatpy import HowLongToBeat
 
 
 class Indexer:
@@ -43,6 +44,23 @@ class Indexer:
         return column_index, row_index
 
 
+    # TODO move excel stuff into this class
+
+
+    def update_cell(self):
+        '''
+        ph
+        '''
+        pass
+
+
+    def view_cell(self):
+        '''
+        ph
+        '''
+        pass
+
+
 class Tracker:
 
     # config init
@@ -59,6 +77,7 @@ class Tracker:
         workbook=games,
         column_name='Game Name',
         column_letter='A')
+    date_format = '%m/%d/%Y'
 
 
     @staticmethod
@@ -84,10 +103,50 @@ class Tracker:
         '''
         Converts minutes to a written string of minutes(1) or hours(1.0).
         '''
-        if playtime_forever < 61:
+        if playtime_forever <= 60:
             return f'{playtime_forever} minutes'
         else:
             return f'{round(playtime_forever/60, 1)} hours'
+
+
+    def get_time_to_beat(self):
+        '''
+        ph
+        '''
+        skip_filled = 1
+        time_to_beat_column_name = 'Time To Beat in Hours'
+        try:
+            for game_name in self.row_index:
+                if skip_filled:
+                    hltb = self.games.cell(row=self.row_index[game_name],
+                        column=self.column_index[time_to_beat_column_name]).value
+                    if hltb != None:
+                        continue
+                start = time.perf_counter()
+                play_status = self.games.cell(row=self.row_index[game_name],
+                    column=self.column_index['Play Status']).value
+                if play_status in ['Unplayed', 'Playing', 'Played']:
+                    results = HowLongToBeat().search(game_name)
+                    if results is not None and len(results) > 0:
+                        best_element = max(results, key=lambda element: element.similarity)
+                        time_to_beat = str(best_element.gameplay_main).replace('½','.5')
+                        time_to_beat_unit = best_element.gameplay_main_unit
+                        if time_to_beat_unit == None:
+                            continue
+                        elif time_to_beat_unit != 'Hours':
+                            time_to_beat = round(float(time_to_beat)/60, 1)  # converts minutes to hours
+                        print(best_element.game_name, time_to_beat, time_to_beat_unit)
+                        # TODO switch to hours only
+                        self.games.cell(row=self.row_index[game_name],
+                            column=self.column_index[time_to_beat_column_name]).value = time_to_beat
+                end = time.perf_counter()
+                print('Time to check', round(end-start, 2))
+                if end-start < 2:
+                    time.sleep(1)
+        except KeyboardInterrupt:
+            print('\nCancelled')
+        finally:
+            self.save_excel_sheet()
 
 
     def format_cells(self, game_name):
@@ -115,42 +174,56 @@ class Tracker:
                 row=self.row_index[game_name], column=self.column_index[cell]).border = border
 
 
-    def refresh_steam_games(self):
+    def refresh_steam_games(self, steam_id):
         '''
         Gets games owned by the entered Steam ID amd runs excel update/add functions.
         '''
         # asks for a steam id if the given one is invalid
-        if len(self.steam_id) != 17:
-            self.steam_id = input('Invalid Steam ID (It must be 17 numbers.)\nTry Again.\n')
+        if len(steam_id) != 17:
+            steam_id = input('Invalid Steam ID (It must be 17 numbers.)\nTry Again.\n')
         root_url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/'
-        url_var = f'?key={self.get_api_key()}&steamid={self.steam_id}'
+        url_var = f'?key={self.get_api_key()}&steamid={steam_id}'
         combinded_url = f'{root_url}{url_var}&include_played_free_games=0&format=json&include_appinfo=1'
-        data = requests.get(combinded_url).json()
-        self.total_games_updated = 0
-        self.total_games_added = 0
-        self.added_games = []
-        for item in data['response']['games']:
-            game_name = item['name']
-            playtime_forever = item['playtime_forever']
-            game_appid = item['appid']
-            # sets play_status to Demo if demo or test appears in the name
-            if re.search(r'\bdemo\b', item['name'].lower()) or re.search(r'\btest\b', item['name'].lower()):
-                play_status = 'Demo'
-            # sets play_status to Unplayed if playtime_forever is 0 minutes
-            elif playtime_forever == 0:
-                play_status = 'Unplayed'
-            # sets play_status to Played if playtime_forever is greater then 10 hours minutes
-            elif item['playtime_windows_forever'] > 10 * 60:  # hours multiplied by minutes in an hour
-                play_status = 'Played'
-            # sets play_status to Unset if none of the above applies
-            else:
-                play_status = 'Unset'
-            if game_name in self.row_index.keys():
-                self.update_game(game_name, playtime_forever, play_status)
-            else:
-                self.add_game(game_name, playtime_forever, game_appid, play_status)
-                self.added_games.append(game_name)
-            self.format_cells(item['name'])
+        try:
+            data = requests.get(combinded_url)
+        except requests.exceptions.ConnectionError:
+            print('Internet Error')
+            return False
+        if data.status_code == requests.codes.ok:
+            self.total_games_updated = 0
+            self.total_games_added = 0
+            self.added_games = []
+            for game in data.json()['response']['games']:
+                game_name = game['name']
+                playtime_forever = game['playtime_forever']
+                game_appid = game['appid']
+                # Demo
+                if re.search(r'\bdemo\b', game['name'].lower()) or re.search(r'\btest\b', game['name'].lower()):
+                    play_status = 'Demo'  # sets play_status to Demo if demo or test appears in the name
+                # Unplayed
+                elif playtime_forever <= 20:
+                    play_status = 'Unplayed'  # sets play_status to Unplayed if playtime_forever is 0-15
+                # Played
+                elif game['playtime_windows_forever'] > 10 * 60:  # hours multiplied by minutes in an hour
+                    play_status = 'Played'  # sets play_status to Played if playtime_forever is greater then 10 hours
+                # Unset
+                else:
+                    play_status = 'Unset'  # sets play_status to Unset if none of the above applies
+                # Updates game if it is in the index or adds if it is not.
+                if game_name in self.row_index.keys():
+                    self.update_game(game_name, playtime_forever, play_status)
+                else:
+                    self.add_game(game_name, playtime_forever, game_appid, play_status)
+                    self.added_games.append(game_name)
+                # formats the added/updated cells to be sure it is set right
+                self.format_cells(game['name'])
+            return True
+        if data.status_code == 500:
+            print('Server Error: make sure your api key and steam id is valid.')
+        else:
+            print('\nFailed to connect to Steam API:\nCheck your internet.')
+            print(data.status_code)
+            return False
 
 
     def save_excel_sheet(self):
@@ -191,12 +264,12 @@ class Tracker:
             self.games.cell(row=self.row_index[game_name],
                 column=self.column_index['Converted Time Played']).value = self.playtime_conv(playtime_forever)
             self.games.cell(row=self.row_index[game_name],
-                column=self.column_index['Last Updated']).value = dt.datetime.now()
+                column=self.column_index['Last Updated']).value = dt.datetime.now().strftime(self.date_format)
             self.total_games_updated += 1
             if play_status == 'unset':
                 self.games.cell(row=self.row_index[game_name], column=self.column_index['Play Status']).value = 'Played'
             added_time = self.playtime_conv(playtime_forever - current_playtime)
-            print(f'\n{game_name} updated.\nAdded {added_time}.')
+            print(f'\n > {game_name} updated.\n   Added {added_time}.')
 
 
     def add_game(self, game_name, playtime_forever, game_appid, play_status):
@@ -214,8 +287,8 @@ class Tracker:
             'Converted Time Played':self.playtime_conv(playtime_forever),
             'App ID':game_appid,
             'Play Status':play_status,
-            'Last Updated':dt.datetime.now().date(),
-            'Date Added':dt.datetime.now().date(),
+            'Last Updated':dt.datetime.now().strftime(self.date_format),
+            'Date Added':dt.datetime.now().strftime(self.date_format),
         }
         append_list = []
         for column in self.column_index:
@@ -234,36 +307,42 @@ class Tracker:
         '''
         Allows you to pick a play_status to have a random game chosen from. It allows retrying.
         '''
-        if input('\nDo you want to pick a random game with a specific play status?').lower() in ['yes', 'y', 'yeah']:
-            print('What play status do you want a random game picked from?')
-            play_status_choices = ['Played', 'Playing', 'Waiting', 'Finished', 'Quit', 'Unplayed', 'Ignore', 'Demo']
-            play_status = input(', '.join(play_status_choices) + '\n')
+        if input('\nDo you want to pick a random game with a specific play status?\n').lower() in ['yes', 'y', 'yeah']:
+            print('\nWhat play status do you want a random game picked from?')
+            play_status_choices = {
+                '1':'Played', '2':'Playing', '3':'Waiting', '4':'Finished',
+                '5':'Quit', '6':'Unplayed', '7':'Ignore', '8':'Demo'
+            }
+            play_status = input(', '.join(play_status_choices.values()) + '\n')
+            if len(play_status) == 1:
+                play_status = play_status_choices[play_status]
             choice_list = []
             for game, index in self.row_index.items():
                 game_play_status = self.games.cell(row=index, column=self.column_index['Play Status']).value.lower()
                 if game_play_status == play_status.lower():
                     choice_list.append(game)
             picked_game = random.choice(choice_list)
-            print(f'\nPicked game:\n{picked_game}')
+            # TODO add improvements to output
+            print(f'\nPicked game with {play_status} status:\n{picked_game}')
             # allows getting another random pick
-            while not input('Press Enter to pick another and No for finish.\n').lower() in ['no', 'n']:
+            while not input('Press Enter to pick another and No for finish.').lower() in ['no', 'n']:
                 picked_game = random.choice(choice_list)
-                print(f'\nPicked game:\n{picked_game}')
+                print(f'\nPicked game with {play_status} status:\n{picked_game}')
 
 
     def run(self):
         '''
         Main run function.
         '''
-        print('Starting Game Tracker Update')
-        self.refresh_steam_games()
-        # shows total games added and updated(includes info that was updated)
-        print(f'\nGames Added: {self.total_games_added}')
-        if len(self.added_games) > 0:
-            added_games_string = ', '.join(self.added_games)
-            print(added_games_string)
-        print(f'\nGames Updated: {self.total_games_updated}')
-        self.save_excel_sheet()
+        print('Starting Game Tracker')
+        if self.refresh_steam_games(self.steam_id):
+            # shows total games added and updated(includes info that was updated)
+            print(f'\nGames Added: {self.total_games_added}')
+            if len(self.added_games) > 0:
+                added_games_string = ', '.join(self.added_games)
+                print(added_games_string)
+            print(f'\nGames Updated: {self.total_games_updated}')
+            self.save_excel_sheet()
         try:
             self.pick_random_game()
             input('\nPress Enter to open updated file in Excel.\n')
@@ -273,4 +352,5 @@ class Tracker:
 
 
 if __name__ == "__main__":
-    Tracker().run()
+    # Tracker().run()
+    Tracker().get_time_to_beat()
