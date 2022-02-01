0f565ed951ff665f3da65aca8e4a042a1ba6f256
Concrete18 <michaelericson19@gmail.com>
2022-02-01 15:48:02 -0500

    small changes

--- ignore_maker/ingore_maker.py
+++ ignore_maker/ingore_maker.py
@@ -1,18 +1,49 @@
-with open('ignore_maker\data.txt') as f:
-    games = f.read().splitlines()
+from pathlib import Path
 
-def prep_for_ignore_list():
-    for game in games:
-        print(f'"{game}",')
 
-def order_and_dupe_remover():
-    found = []
-    for game in games:
-        if game not in found:
-            found.append(game)
-    found.sort()
-    for game in found:
-        print(game)
+class Ignore_Maker():
 
-prep_for_ignore_list()
-# order_and_dupe_remover()
+
+    games = Path('ignore_maker\data.txt').read_text().splitlines()
+
+
+    def prep_for_ignore_list(self):
+        '''
+        ph
+        '''
+        unicode_issues = []
+        for game in self.games:
+            if r'\u' in game:
+                unicode_issues.append(game)
+            else:
+                print(f'"{game}",')
+        if len(unicode_issues) > 0:
+            print('The following have unicode issues.')
+            for game in unicode_issues:
+                print(game)
+
+    def order_and_dupe_remover(self):
+        '''
+        ph
+        '''
+        found = []
+        for game in self.games:
+            if game not in found:
+                found.append(game)
+        found.sort()
+        for game in found:
+            print(game)
+
+    def run(self):
+        '''
+        ph
+        '''
+        first_entry = self.games[0]
+        if first_entry.startswith('"') and first_entry.endswith('",'):
+            self.order_and_dupe_remover()
+        else:
+            self.prep_for_ignore_list()
+
+
+app = Ignore_Maker()
+app.run()
