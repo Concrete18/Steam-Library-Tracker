debb707bc4cfb1673e1fc41cd2836166208a7fa3
Concrete18 <michaelericson19@gmail.com>
2022-02-01 15:49:49 -0500

    added unicode remover

--- classes/helper.py
+++ classes/helper.py
@@ -4,7 +4,7 @@ import time
 class Helper:
 
 
-    def api_sleeper(self, api, sleep_length=.5, api_calls={}):
+    def api_sleeper(self, api, sleep_length=.5, api_calls={}) -> None:
         '''
         Delays delays for a set period of time if the `api` was run too recently.
         Delay length is set by `sleep_length`.
@@ -14,3 +14,31 @@ class Helper:
             if api_calls[api] + dt.timedelta(seconds=sleep_length) > cur_datetime:
                 time.sleep(sleep_length)
         api_calls[api] = cur_datetime
+    
+    def request_retryer():
+        pass
+
+    @staticmethod
+    def unicode_remover(string) -> str:
+        '''
+        ph
+        '''
+        if type(string) != str:
+            return string
+        replace_dict = {
+            '\u2122': '',  # Trademarked sign
+            '\u00ae': '',  # REGISTERED SIGN
+            '\u00ae': '',  # REGISTERED SIGN
+            '\u00e5': 'a',  # a
+            '\u00f6': 'o',  # LATIN SMALL LETTER O WITH DIAERESIS
+            '\u00e9': 'e',  # LATIN SMALL LETTER E WITH ACUTE
+            '\u2013': '-',  # EN DASH
+            '&amp': '&',  # &
+            }
+        for unicode in replace_dict.keys():
+            if unicode in string:
+                for unicode, sub in replace_dict.items():
+                    string = string.replace(unicode, sub)
+                break
+        conv_string = string.encode("ascii", "ignore").decode()
+        return conv_string.strip()
