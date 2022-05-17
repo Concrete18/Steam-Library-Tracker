e3da8bd9838963df901733caf6e783e61b5cc9fa
Concrete18 <michaelericson19@gmail.com>
2022-05-16 23:03:23 -0400

    improved ask_for_integer func

--- classes/helper.py
+++ classes/helper.py
@@ -160,13 +160,32 @@ class Helper(Logger):
         conv_string = string.encode("ascii", "ignore").decode()
         return conv_string.strip()
 
-    def ask_for_integer(self, msg="Type a Number: ") -> int:
+    def ask_for_integer(
+        self, msg=None, num_range=False, allow_blank=False
+    ) -> int or bool:
         """
         Asks for a integer until an integer is given.
         """
+        if msg is None:
+            msg = "Type a Number: "
         num = input(msg)
-        while not num.isdigit():
-            num = input(msg)
+        if allow_blank and num == "":
+            return ""
+        if num_range:
+            min = num_range[0]
+            max = num_range[1]
+            while True:
+                if num.isdigit():
+                    if min <= int(num) <= max:
+                        break
+                num = input(msg)
+                if allow_blank and num == "":
+                    return ""
+        else:
+            while not num.isdigit():
+                num = input(msg)
+                if allow_blank and num == "":
+                    return ""
         return int(num)
 
     def lev_distance(self, word1: str, word2: str, lower=True) -> int:
