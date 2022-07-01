b17d20f603e18aa477ffb34a0d79ee7d84c1f22c
Concrete18 <michaelericson19@gmail.com>
2022-06-30 22:38:14 -0400

    added word_and_list function

--- classes/helper.py
+++ classes/helper.py
@@ -179,6 +179,31 @@ class Helper(Logger):
         conv_string = string.encode("ascii", "ignore").decode()
         return conv_string.strip()
 
+    @staticmethod
+    def word_and_list(str_list) -> str:
+        """
+        Converts a string into a comma seperated string of words
+        with "and" instead of a comma between the last two entries.
+        """
+        length = len(str_list)
+        if length == 1:
+            return str_list[0]
+        elif length == 2:
+            return f"{str_list[0]} and {str_list[1]}"
+        final_string = ""
+        for i, entry in enumerate(str_list):
+            # first entry
+            if i == 0:
+                final_string += entry
+                continue
+            # last entry
+            elif i == length - 1:
+                final_string += f" and {entry}"
+                return final_string
+            # middle entries
+            else:
+                final_string += f", {entry}"
+
     def ask_for_integer(
         self, msg=None, num_range=False, allow_blank=False
     ) -> int or bool:
@@ -297,3 +322,4 @@ if __name__ == "__main__":
     # response = App.request_url("https://store.steampowered.com/app/752564654590/")
     # print(response.url)
     # print(App.unicode_remover("Half-Life 2: Lost Coast"))
+    print(App.word_and_list(["Test1", "test2", "test3"]))
