cf158123c358df4b840068d449e5707c340d26cc
Concrete18 <michaelericson19@gmail.com>
2024-05-27 16:16:42 -0400

    improved backup tests

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -8,15 +8,9 @@ class TestBackup:
     backup = Backup(file="tests/readme.md")
 
     def test_bool(self):
-        """
-        ph
-        """
         assert self.backup
 
     def test_eq(self):
-        """
-        ph
-        """
         backup2 = Backup(file="test")
         # same file is identical to itself
         assert self.backup == self.backup
@@ -31,6 +25,15 @@ class TestBackup:
     def test_path_not_string(self):
         with pytest.raises(TypeError):
             Backup(file=3211)
+
+
+class TestCreateBackupPath:
+    backup = Backup(file="tests/readme.md")
+
+    def test_success(self):
+        backup_path = self.backup.create_backup_path()
+        assert str(backup_path).startswith("backup\\readme_")
+        assert str(backup_path).endswith(".md")
 
 
 if __name__ == "__main__":
